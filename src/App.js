889bca16b2097a49c211c25b731e56e22962ec8d
farzanachowdhury <farzanac@umich.edu>
2025-12-10 22:24:13 -0500

    emoji and name added

--- src/App.js
+++ src/App.js
@@ -1,30 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
 import TestComponent from "./components/TestComponent";
+import {useState} from "react";
 
 const Comp = () => {
-  return <p>This is a comp component</p>
+  return <p>My name is Farzana and my uniqname is farzanac</p>
 }
 
 function App() {
+    const [count, setCount] = useState(2);
   return (
     <div className="App" id='root'>
             <TestComponent />
 
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <Comp />
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+          <button
+          onClick={() => setCount(count + 1)}>
+              Click Me!
+          </button>
+          <div>{'🥸'.repeat(count)}</div>
+
       </header>
     </div>
   );
